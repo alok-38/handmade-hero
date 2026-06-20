337a171d21c73618381fd7e0a39d681af42556f3
Alokananda Y <plantsfortrees@gmail.com>
2026-06-20 12:14:38 +0530

    Studying win32 API

--- tutorials/hello.c
+++
@@ -1,7 +0,0 @@
-#include <windows.h>
-
-int main()
-{
-    MessageBox(NULL, "Hello, World!", "My App", MB_OK);
-    return 0;
-}
---
+++ tutorials/win32_console.c
@@ -0,0 +1,13 @@
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+
+#define WINVER 0x0A00
+#define _WIN32_WINNT 0x0A00
+#define NTDDI_VERSION 0x0A000000
+
+#include <windows.h>
+
+int main(void)
+{
+    return 0;
+}
